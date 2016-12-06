ce48c2382aead29b6dab102b4f7facfa5453a821
Phil G <philg@Phils-MacBook-Pro.local>
2016-12-06 16:50:07 -0600

    everything added to views

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,29 +1,29 @@
-// var animalAPI = angular.module('animalAPI', ['ngRoute']);
-//
-// animalAPI.config(['$routeProvider', function($routeProvider) {
-//
-//     $routeProvider
-//         .when('/picker', {
-//             templateUrl: '/views/templates/picker.html',
-//             controller: 'pickerController',
-//             controllerAs: 'picker'
-//         })
-//         .when('/logIn', {
-//             templateUrl: '/views/templates/logIn.html',
-//             controller: 'logInController',
-//             controllerAs: 'logIn'
-//         })
-//         .when('/options', {
-//             templateUrl: '/views/templates/options.html',
-//             controller: 'optionsController',
-//             controllerAs: 'options'
-//         })
-//         .when('/youtubeResults', {
-//             templateUrl: '/views/templates/youtubeResults.html',
-//             controller: 'resultsController',
-//             controllerAs: 'results'
-//         })
-//         .otherwise({
-//             redirectTo: '/logIn'
-//         });
-// }]);
+var youtubeAPI = angular.module('youtubeAPI', ['ngRoute']);
+
+youtubeAPI.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider
+        .when('/picker', {
+            templateUrl: '/views/templates/picker.html',
+            controller: 'pickerController',
+            controllerAs: 'picker'
+        })
+        .when('/logIn', {
+            templateUrl: '/views/templates/logIn.html',
+            controller: 'logInController',
+            controllerAs: 'logIn'
+        })
+        .when('/options', {
+            templateUrl: '/views/templates/options.html',
+            controller: 'optionsController',
+            controllerAs: 'options'
+        })
+        .when('/youtubeResults', {
+            templateUrl: '/views/templates/youtubeResults.html',
+            controller: 'resultsController',
+            controllerAs: 'results'
+        })
+        .otherwise({
+            redirectTo: '/logIn'
+        });
+}]);
