311b6cf2edd9cc364b9eac184ccb4b3f3f19999e
Phil G <philg@Phils-MacBook-Pro.local>
2016-12-12 17:03:56 -0600

    cleaned up index

---
+++ server/public/scripts/dropdown.js
@@ -0,0 +1,8 @@
+(function() {
+    var customSelects = document.querySelectorAll(".custom-dropdown__select");
+    for (var i = 0; i < customSelects.length; i++) {
+        if (customSelects[i].hasAttribute("disabled")) {
+            customSelects[i].parentNode.className += " custom-dropdown--disabled";
+        }
+    }
+})()
