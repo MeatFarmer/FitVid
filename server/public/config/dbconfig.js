c22ce93e70ab44e3c169b8a4fef8a2a03572c047
Phil G <philg@Phils-MacBook-Pro.local>
2017-01-09 09:00:19 -0600

    adjusting heroku db url

--- server/public/config/dbconfig.js
+++ server/public/config/dbconfig.js
@@ -5,13 +5,11 @@ var config = {
 //
 // If we are running on Heroku, use the remote database (with SSL)
 if(process.env.DATABASE_URL != undefined) {
-    config.database = process.env.DATABASE_URL + "?ssl=true";
+    config.database = process.env.DATABASE_URL; // + "?ssl=true";
 } else {
     // running locally, use our local database instead
     config.database = 'sigma';
 }
-
-
 
 
 module.exports = config;
