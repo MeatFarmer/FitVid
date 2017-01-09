7e0187cd3d6e4bee57fa8e8ffa67dd76375a0414
Phil G <philg@Phils-MacBook-Pro.local>
2017-01-09 09:40:31 -0600

    added heroku config var parsing for pg pool connection

--- server/public/config/dbconfig.js
+++ server/public/config/dbconfig.js
@@ -5,6 +5,7 @@ var config = {};
 // If we are running on Heroku, use the remote database (with SSL)
 
 if(process.env.DATABASE_URL != undefined) {
+  // postgres://txgxscxzmadzjd:1d43f55f7b6fe8672d3510d5c0ee302497c64c7b9677c3924923780eb1abb7f8@ec2-23-21-219-105.compute-1.amazonaws.com:5432/dbl3itb0p4mmtd
   var params = url.parse(process.env.DATABASE_URL);
   var auth = params.auth.split(':');
 
@@ -16,6 +17,8 @@ if(process.env.DATABASE_URL != undefined) {
     database: params.pathname.split('/')[1],
     ssl: true
   };
+
+  console.log('DB CONFIG: ', config);
 
 } else {
   // running locally, use our local database instead
